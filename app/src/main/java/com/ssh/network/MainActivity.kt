87c6542ad70a7c8bd4509d70766a754afbb0a3a6
soong <iamssh@126.com>
2019-07-16 16:53:54 +0800

    MainActivity

--- app/src/main/java/com/ssh/network/MainActivity.kt
+++ app/src/main/java/com/ssh/network/MainActivity.kt
@@ -9,4 +9,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    private val url = "$host/signup"
+//
+//    fun signUpRequest(name: String, email: String, teamwork: String, password: String,
+//                      failure: (httpException: HttpException) -> Unit,
+//                      success: (data: String, code: Int) -> Unit) {
+//
+//        val mMap = HashMap<String, String>()
+//        mMap["name"] = name
+//        mMap["email"] = email
+//        mMap["teamwork"] = teamwork
+//        mMap["password"] = password
+//        mMap["key"] = key
+//
+//        NetManger.getRequest(OKHttpRequest::class.java).doPost(url, mMap, object : IResponseListener {
+//            override fun onResponse(response: String, code: Int) {
+//                success(response, code)
+//            }
+//
+//            override fun onFail(httpException: HttpException) {
+//                failure(httpException)
+//            }
+//        })
+//
+//    }
 }
